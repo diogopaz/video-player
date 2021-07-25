e79b51952ede53c54b85f2f97f67f05c588114ce
Diogo Paz <diogoachiles@gmail.com>
2021-07-24 22:37:41 -0400

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+let video = document.getElementsByClassName("video")[0];
+let playBtn = document.getElementsByClassName("play")[0];
+let pauseBtn = document.getElementsByClassName("pause")[0];
+let controls = document.getElementsByClassName("controls")[0];
+let videoInProgress = false;
+
+function play() {
+    video.play();
+    pauseBtn.removeAttribute("hidden");
+    playBtn.setAttribute("hidden", "");
+    videoInProgress = true;
+}
+
+function pause() {
+    video.pause();
+    pauseBtn.setAttribute("hidden", "");
+    playBtn.removeAttribute("hidden");
+    videoInProgress = false;
+}
+
+function stop() {
+    video.pause();
+    video.currentTime = 0;
+    pauseBtn.setAttribute("hidden", "");
+    playBtn.removeAttribute("hidden");
+    videoInProgress = false;
+}
+
+function rewind() {
+    video.currentTime -= 10;
+}
+
+function advance() {
+    video.currentTime += 10;
+}
+
+function decreaseSpeed() {
+    video.playbackRate -= 0.25;
+}
+
+function increaseSpeed() {
+    video.playbackRate += 0.25;
+}
+
+function hideControls() {
+    if (videoInProgress) {
+        setTimeout(function () {
+            controls.setAttribute("hidden", "");
+        }, 2500);
+    }
+}
+
+function showControls() {
+    controls.removeAttribute("hidden");
+}
+
+video.addEventListener("mouseout", hideControls);
+video.addEventListener("mouseover", showControls);
